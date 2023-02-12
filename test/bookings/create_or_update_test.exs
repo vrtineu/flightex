a8d84578c8813eb9116888bad4addc06e27a528d
vrtineu <vininro@gmail.com>
2023-02-12 20:38:53 -0300

    test(booking): creating error case tests

--- test/bookings/create_or_update_test.exs
+++ test/bookings/create_or_update_test.exs
@@ -15,7 +15,7 @@ defmodule Flightex.Bookings.CreateOrUpdateTest do
         complete_date: ~N[2001-05-07 03:05:00],
         local_origin: "Brasilia",
         local_destination: "Bananeiras",
-        user_id: "e9f7d281-b9f2-467f-9b34-1b284ed58f9e",
+        user_id: "e9f7d281-b9f2-467f-9b34-1b284ed58f9e"
       }
 
       {:ok, uuid} = CreateOrUpdate.call(params)
@@ -29,6 +29,21 @@ defmodule Flightex.Bookings.CreateOrUpdateTest do
         local_origin: "Brasilia",
         user_id: "e9f7d281-b9f2-467f-9b34-1b284ed58f9e"
       }
+
+      assert response == expected_response
+    end
+
+    test "when params are invalid, returns an error tuple" do
+      params = %{
+        complete_date: "2001-05-07",
+        local_origin: "Brasilia",
+        local_destination: "Bananeiras",
+        user_id: "e9f7d281-b9f2-467f-9b34-1b284ed58f9e"
+      }
+
+      expected_response = {:error, "complete_date must be a NaiveDateTime"}
+
+      response = CreateOrUpdate.call(params)
 
       assert response == expected_response
     end
