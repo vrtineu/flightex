48d0e9759c14f598d3a529663bb1a60f1b30c117
vrtineu <vininro@gmail.com>
2023-02-12 20:02:29 -0300

    feat: creating facade module

--- lib/flightex.ex
+++ lib/flightex.ex
@@ -1,3 +1,18 @@
 defmodule Flightex do
+  alias Flightex.Bookings.Agent, as: BookingAgent
+  alias Flightex.Bookings.CreateOrUpdate, as: CreateOrUpdateBooking
+  alias Flightex.Bookings.Report
+  alias Flightex.Users.Agent, as: UserAgent
+  alias Flightex.Users.CreateOrUpdate, as: CreateOrUpdateUser
 
+  def start_agents do
+    UserAgent.start_link(%{})
+    BookingAgent.start_link(%{})
+  end
+
+  defdelegate create_or_update_user(params), to: CreateOrUpdateUser, as: :call
+  defdelegate create_or_update_booking(params), to: CreateOrUpdateBooking, as: :call
+
+  defdelegate generate(filename \\ "report.csv"), to: Report, as: :generate
+  defdelegate generate_report(from_date, to_date), to: Report, as: :generate_report
 end
